57bd6456aaa73b34e5656f1297b8a983552eb4ff
wutong <wt01278@163.comm>
2018-06-07 23:28:08 +0800

    commit

--- springProject-web/src/test/java/RedisDaoTest.java
+++ springProject-web/src/test/java/RedisDaoTest.java
@@ -1,29 +1,21 @@
-import com.wt.dao.RedisDao;
-import com.wt.model.T_USER;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.Jedis;
 
-import javax.annotation.Resource;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath*:applicationContext.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("classpath*:applicationContext.xml")
 public class RedisDaoTest {
 
-    @Autowired
-    private RedisDao redisDao;
+//    @Autowired
+//    private RedisDao redisDao;
 
     @Test
     public void test(){
-        redisDao.save();
-        System.out.println(redisDao.getUser(1));
-
-//        Jedis jedis=new Jedis("127.0.0.1",6397);
+//        redisDao.save();
+//        System.out.println(redisDao.getUser(1));
+//
+        Jedis jedis=new Jedis("127.0.0.1",6397);
 //        jedis.auth("wutong");
-//        System.out.println(jedis.ping());
+        System.out.println(jedis.ping());
     }
 
 
