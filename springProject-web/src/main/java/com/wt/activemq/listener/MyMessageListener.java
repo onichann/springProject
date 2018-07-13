da57595f50700f7caab5634097fcbfdaf1e60374
wutong <wt01278@163.comm>
2018-07-13 13:44:21 +0800

    commit

--- springProject-web/src/main/java/com/wt/activemq/listener/MyMessageListener.java
+++ springProject-web/src/main/java/com/wt/activemq/listener/MyMessageListener.java
@@ -1,5 +1,7 @@
 package com.wt.activemq.listener;
 
+import com.wt.exception.SpringWebException;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -14,9 +16,13 @@ public class MyMessageListener implements MessageListener {
             String text = textMessage.getText();
             // 打印消息
             System.out.println("activeMQ监听器监听到消息:"+text);
+            //只要被确认后  就会出队，接受失败没有确认成功，会在原队列里面
+//            textMessage.acknowledge();
         } catch (JMSException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        throw new SpringWebException("抛出异常,模拟重发");
     }
 }
